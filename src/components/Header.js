bda2a8765b34b005dd2ca1f40a010b893ac730bd
binyamyohannes <olangobinyam@gmail.com>
2023-12-28 15:28:35 +0300

    Add logo img

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,9 @@ const Header = () => {
     <header className="header">
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand as={Link} to="/"><img src="../assets/leaderboard-logo.png" alt="logo-img" /></Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            <img className="logo-img" src="https://static.thenounproject.com/png/3388492-200.png" alt="logo-img" />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
